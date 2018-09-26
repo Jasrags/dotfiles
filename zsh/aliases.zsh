dddfa6c96827427958d6fe92cf67e89ef58c6cf0
Jason Ragsdale <jasonr@imgur.com>
2018-09-26 10:33:48 -0500

    Updated dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,7 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias gfp='git fetch -p'
+
+alias kickwifi='networksetup -getairportpower en0 | grep "On" && networksetup -setairportpower en0 off && networksetup -setairportpower en0 on'
+
+alias openhere='open -a /Applications/iTerm.app .'
